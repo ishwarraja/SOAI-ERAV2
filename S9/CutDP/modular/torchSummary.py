f667d46067aa4c52e5f4a7965712ff6888099ca7
ishwarraja <71460569+ishwarraja@users.noreply.github.com>
2023-07-01 23:23:40 +0530

    Add files via upload

---
+++ S9/CutDP/modular/torchSummary.py
@@ -0,0 +1,11 @@
+
+
+from torchsummary import summary
+
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+print(device)
+model = Net().to(device)
+summary(model, input_size=(3, 32, 32))
+
+
